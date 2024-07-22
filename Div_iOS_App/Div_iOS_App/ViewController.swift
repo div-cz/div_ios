c64a989049d93e028d176d11611222e2435c699c
Christian <kiko22223@gmail.com>
2024-07-22 22:13:18 +0200

    New files made for app

---
+++ Div_iOS_App/Div_iOS_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Div_iOS_App
+//
+//  Created by Christián on 22/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
